032460904cb13acede7878937b3be9ffb1f1fd2b
Ewelina <106283972+Valentine0604@users.noreply.github.com>
2022-09-15 23:00:31 +0200

    Part 2 of the Dealership app.
    Option "a" was developed.

--- Dealership.java
+++ Dealership.java
@@ -10,7 +10,31 @@ public class Dealership {
 
         switch(option){
             case "a":
-                System.out.println("You have chosen option " + option);
+                System.out.println("What is your budget?");
+                int budget = scan.nextInt();
+
+                if(budget >= 10000){
+                    System.out.println("Great! A Nissan Altima is available");
+
+                    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+                    String insurance = scan.next();
+
+                    System.out.println("\nDo you have a driver license? Write 'yes' or 'no'");
+                    String license = scan.next();
+
+                    System.out.println("\nWhat is your credit score?");
+                    int creditScore = scan.nextInt();
+
+                    if(insurance.equals("yes") && license.equals("yes") && creditScore > 6600){
+                        System.out.println("\nSold! Pleasure doing business with you");
+                    }
+                    else{
+                        System.out.println("\nWe're sorry. You are not eligible");
+                    }
+                }
+                else{
+                    System.out.println("We don't sell cars under $10000. Sorry!");
+                }
                 break;
             case "b":
                 System.out.println("You have chosen option " + option);
